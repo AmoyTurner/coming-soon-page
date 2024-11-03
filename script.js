84155530ff1a01971794dd8788cd024e85b2529f
AmoyTurner <amoyturner4@gmail.com>
2024-11-03 15:35:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Countdown Timer Script
+const countdown = () => {
+    const launchDate = new Date("Dec 31, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const distance = launchDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("timer").innerHTML = "We're Live!";
+    }
+};
+
+const interval = setInterval(countdown, 1000);
